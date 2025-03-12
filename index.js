1a984f0e2676dd5fa6f9b48d2e567c59a450d0be
francollovera <francollovera@gmail.com>
2025-03-12 17:18:18 -0300

    back

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+// SDK de Mercado Pago
+import { MercadoPagoConfig, Preference } from "mercadopago";
+// Agrega credenciales
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Soy el server :)");
+});
+
+app.post("/create_preference", async (req, res) => {
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.price),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: "https://www.youtube.com/@onthecode",
+        failure: "https://www.youtube.com/@onthecode",
+        pending: "https://www.youtube.com/@onthecode",
+      },
+      auto_return: "approved",
+    };
+
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error al crear la preferencia :(",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`El servidor esta corriendo en el puerto ${port}`);
+});
